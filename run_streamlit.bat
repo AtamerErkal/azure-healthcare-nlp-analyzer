e59827c8f1c873bd30ae13c15cbee902196f1048
AtamerErkal <atamererkal.eu@gmail.com>
2026-02-21 13:57:52 +0100

    Update .bat: fit: new UI

--- run_streamlit.bat
+++ run_streamlit.bat
@@ -1,3 +1,3 @@
 @echo off
 REM Run Streamlit using current Python (avoids broken launcher when streamlit.exe points to wrong Python)
-python -m streamlit run ui/streamlit_demo.py %*
+python -m streamlit run ui/streamlit_demo_v1.py %*
